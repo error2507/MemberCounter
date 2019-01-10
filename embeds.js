6962d8ffb0ae7f53ebab075d4901b8fc3f63c774
error2507 <37834770+error2507@users.noreply.github.com>
2019-01-10 19:53:40 +0100

    Initial

---
+++ embeds.js
@@ -0,0 +1,67 @@
+let discord = require('discord.js');
+module.exports = {
+    help(tag) {
+        let embed = new discord.RichEmbed()
+            .setColor(0x00bcd4)
+            .setTitle('Help')
+            .setDescription('On join I automatically set my nickname to the actual membercount (with all bots). When a member leaves or joins I also update my nickname. **Tip:** set my role as hoisted and place it at the top so it is easier to see. Therefore I don\'t need much commands. But if you think my nickname isn\'t showing the correct amount, you can use the following command to manually update my nickname.')
+            .addField('%update', 'With this command I update my nickname.')
+            .addField('%invite', 'Shows the link to invite me to your server and the invite link to my support server.')
+            .addField('%stats', 'Shows the bots stats on discordbots.org.')
+            .setFooter('Bot coded by ' + tag);
+        return embed;
+    },
+
+    invite(tag) {
+        let embed = new discord.RichEmbed()
+            .setColor(0x3f51b5)
+            .setTitle('Invite Links')
+            .addField('Invite me to your server (Please make sure I get all the permissions I need)', 'https://discordapp.com/api/oauth2/authorize?client_id=448845486515027970&permissions=67110912&scope=bot')
+            .addField('My support server', 'https://discord.gg/nGu44pF')
+            .setFooter('Bot coded by ' + tag);
+        return embed;
+    },
+
+    stats(random) {
+        let embed = new discord.RichEmbed()
+            .setColor(0x00bcd4)
+            .setTitle('Stats')
+            .setImage('https://discordbots.org/api/widget/448845486515027970.png?random=' + random)
+            .setFooter('If you believe the stats aren\'t correct, just reuse this command.')
+        return embed;
+    },
+
+    update: {
+        success(membercount) {
+            let embed = new discord.RichEmbed()
+                .setColor(0x4caf50)
+                .setTitle('Success')
+                .setDescription(`I successfully changed my nickname to ${membercount}`);
+            return embed;
+        },
+
+        missingPerms() {
+            let embed = new discord.RichEmbed()
+                .setColor(0xf44336)
+                .setTitle('Missing permissions')
+                .setDescription('In order to work I need following permission: `CHANGE_NICKNAME` or `ADMINISTRATOR`');
+            return embed;
+        },
+
+        cooldown() {
+            let embed = new discord.RichEmbed()
+                .setColor(0xf44336)
+                .setTitle('Cooldown')
+                .setDescription('Please don\'t spam this command. You can use it again 10 minutes after last usage of the command on this server.');
+            return embed;
+        },
+
+        dm() {
+            let embed = new discord.RichEmbed()
+                .setColor(0xf44336)
+                .setTitle('This command is not working in DMs')
+                .setDescription('You can only use this command on server I am on but not via direct messages.');
+            return embed;
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,132 @@
+const Discord = require('discord.js');
+const DBL = require("dblapi.js");
+const client = new Discord.Client();
+const config = require('./config.json');
+const dbl = new DBL(config.dblToken);
+const embeds = require('./embeds.js');
+const cooldown = new Set();
+
+
+client.on('ready', function(){
+    console.log("MemberCounter is ready!");
+    dbl.postStats(client.guilds.size);
+    setInterval(function() {
+        dbl.postStats(client.guilds.size);
+    }, 300000);
+    client.user.setActivity('%help', { type: 'PLAYING' });
+})
+client.on('message', msg => {
+    var cont = msg.content;
+    if (cont.startsWith(config.prefix) && !msg.author.bot) {
+        var invoke = cont.split(' ')[0].substr(config.prefix.length),
+            args   = cont.split(' ').slice(1);
+
+        if (invoke in cmdmap) {
+            cmdmap[invoke](msg, args);
+        }
+    }   
+});
+
+var cmdmap = {
+    update: cmd_update,
+    help: cmd_help,
+    invite: cmd_invite,
+    stats: cmd_stats,
+    oeval: cmd_oeval,
+    eval: cmd_eval
+}
+
+async function cmd_update(msg, args) {
+    if (msg.channel.type == "dm") {
+        msg.channel.send(embeds.update.dm());
+    } else {
+        await msg.guild.fetchMember(client.user);
+        if (cooldown.has(msg.guild.id)) {
+            msg.channel.send(embeds.update.cooldown());
+        } else {
+            if ((msg.guild.me.hasPermission("CHANGE_NICKNAME") || msg.guild.me.hasPermission("ADMINISTRATOR")) && msg.guild.id != 403269713368711190) {
+                msg.channel.send(embeds.update.success(msg.guild.memberCount))
+                msg.guild.me.setNickname(msg.guild.memberCount);
+                cooldown.add(msg.guild.id)
+                setTimeout(() => {
+                    cooldown.delete(msg.guild.id);
+                }, 600000);
+            } else {
+                msg.channel.send(embeds.update.missingPerms());
+            }
+        }
+    }
+}
+
+function cmd_help(msg, args) {
+    msg.channel.send(embeds.help(client.users.get('403269713368711190').tag));
+}
+
+function cmd_invite(msg, args) {
+    msg.channel.send(embeds.invite(client.users.get('403269713368711190').tag));
+}
+
+function cmd_stats(msg) {
+    let charset = ['e', 'r', 'o', '2', '5', '0', '7', 'm', 'b', 'c', 'u', 'n', 't'];
+    let random = '';
+    for (d = 0; d < 6; d++) {
+        random = random + charset[Math.floor(Math.random() * charset.length)];
+    }
+    msg.channel.send(embeds.stats(random));
+}
+
+function cmd_oeval(msg, args) {
+    if (msg.author.id == 403269713368711190) {
+        try{
+                msg.channel.send("**Input:**\n```" + args.join(" ") + "```\n\n**Output:**\n```" + eval(args.join("")) + "```")
+            } catch(err) {
+                msg.channel.send("**Input:**\n```" + args.join(" ") + "```\n\n**Output:**\n```" + err + "```")
+            }
+        
+    }
+}
+
+function cmd_eval(msg, args) {
+    if (msg.author.id == 403269713368711190) {
+        try{
+                eval(args.join(" "))
+            } catch(err) {
+                msg.channel.send("**Input:**\n```" + args.join(" ") + "```\n\n**Output:**\n```" + err + "```")
+            }
+        
+    }
+}
+
+//Auto change nickname
+client.on('guildCreate', function(newguild){
+    if (newguild.id != 387812458661937152) {
+        if (newguild.me.hasPermission("CHANGE_NICKNAME") || newguild.me.hasPermission("ADMINISTRATOR")) {
+            newguild.me.setNickname(newguild.memberCount);
+        } else {
+            newguild.owner.user.send("Hey! I saw someone added me to " + newguild.name + ". I am pretty excited to show how many members are on there. But to do so I need to have the permission to change my nickname. Please give that permission to me.");
+        }
+    }
+    client.users.get("403269713368711190").send(`I joined **${newguild.name}** with **${newguild.memberCount}** members.`);
+});
+
+client.on('guildMemberRemove', async function(oldmember){
+    await oldmember.guild.fetchMember(client.user);
+    if (oldmember.guild.id != 387812458661937152) {
+        if (oldmember.guild.me.hasPermission("CHANGE_NICKNAME") || oldmember.guild.me.hasPermission("ADMINISTRATOR")) {
+            oldmember.guild.me.setNickname(oldmember.guild.memberCount);
+        }
+    }
+});
+
+client.on('guildMemberAdd', async function(newmember){
+    await newmember.guild.fetchMember(client.user);
+    if (newmember.guild.id != 387812458661937152) {
+        if (newmember.guild.me.hasPermission("CHANGE_NICKNAME") || newmember.guild.me.hasPermission("ADMINISTRATOR")) {
+            newmember.guild.me.setNickname(newmember.guild.memberCount);
+        } 
+    }
+});
+
+
+
+client.login(config.token);
