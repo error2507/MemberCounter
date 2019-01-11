6dcbd58fbbe61af9c6bd93cdb983ff64ca2229c6
error2507 <37834770+error2507@users.noreply.github.com>
2019-01-11 20:17:40 +0100

    Add forgotten async to memmberRemove and Add

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,4 +1,4 @@
-module.exports.run = (oldmember, client) => {
+module.exports.run = async (oldmember, client) => {
     await oldmember.guild.fetchMember(client.user);
     if (oldmember.guild.me.hasPermission("CHANGE_NICKNAME") || oldmember.guild.me.hasPermission("ADMINISTRATOR")) {
         oldmember.guild.me.setNickname(oldmember.guild.memberCount);
