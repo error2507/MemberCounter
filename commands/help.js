cd678fcbf1b0547292aa4d6bf920b7b790b1967f
zekroTJA <riho@live.de>
2019-01-16 15:34:05 +0100

    added some error-resistency... ;)

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,4 @@
 module.exports.run = (msg, args, client) => {
-    msg.channel.send(client.embeds.help(client.users.get('403269713368711190').tag));
-}
+    let user = client.users.get('403269713368711190');
+    msg.channel.send(client.embeds.help(user ? user.tag : "error2507"));
+};
