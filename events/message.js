c0ab5d8743548cae5e1d5a34bc281463ca063819
error2507 <37834770+error2507@users.noreply.github.com>
2019-01-17 14:33:30 +0100

    Made commands not case-sensetive

--- events/message.js
+++ events/message.js
@@ -1,9 +1,9 @@
 module.exports.run = (msg, client) => {
     let prefix = (client.config.debug.enabled == true ? client.config.debug.prefix : client.config.prefix);
     if (msg.content.startsWith(prefix) && !msg.author.bot) {
-        if (client.commands.has(msg.content.split(' ')[0].substr(prefix.length))) {
+        if (client.commands.has(msg.content.split(' ')[0].substr(prefix.length).toLowerCase())) {
             let args = msg.content.split(' ').slice(1);
-            client.commands.get(msg.content.split(' ')[0].substr(prefix.length)).run(msg, args, client);
+            client.commands.get(msg.content.split(' ')[0].substr(prefix.length).toLowerCase()).run(msg, args, client);
         }
     }
 }
