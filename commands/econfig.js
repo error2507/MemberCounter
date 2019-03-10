c97cb42e483cb07ff42129256818bbb39dde841f
error2507 <37834770+error2507@users.noreply.github.com>
2019-03-10 20:06:50 +0100

    Added the option to change the format via reaction based setup + message await

--- commands/econfig.js
+++ commands/econfig.js
@@ -1,3 +1,4 @@
+const utils = require('../utils');
 const stdConfig = {
     'format':    '%all%',
     'countBots': 1,
@@ -21,6 +22,8 @@ module.exports.run = async (msg, args, client) => {
                     if (reactions.size == 1) {
                         switch (reactions.first().emoji.name) {
                             case '👁':
+                                // Removing all reactions
+                                cMsg.clearReactions();
                                 // Getting current guilds config from database.
                                 client.db.getGuildConfig(msg.guild).then((guildConfig) => {
                                     // Check which keys are set in the config. If a key's value
@@ -36,10 +39,45 @@ module.exports.run = async (msg, args, client) => {
                                         guildConfig = stdConfig;
                                     // Edit cMsg (which was the menu before) to display current guild config
                                     cMsg.edit(client.embeds.config.list(guildConfig))
+                                        .then(() => {
+                                            cMsg.delete(30000);
+                                            msg.delete(30000);
+                                        })
                                         .catch((err) => console.error("[ ERROR ] ", err));
                                 })
                                 break;
                             case '✏':
+                                cMsg.clearReactions();
+                                cMsg.edit(client.embeds.config.enterFormat());
+                                let filter = m => m.author.id == msg.author.id;
+                                msg.channel.awaitMessages(filter, { max: 1, time: 60000 })
+                                    .then(msgs => {
+                                        if (msgs.size == 1) {
+                                            let fMsg = msgs.first();
+                                            if ((fMsg.content.includes("%all%") || fMsg.content.includes("%online%")) && !fMsg.content.includes("\"")) {
+                                                // All values after the first command argument will be interpreted as
+                                                // config value to set for specified config key.
+                                                let cfgValue = fMsg.content;
+                                                // Transform config value by keys type transformation function.
+                                                cfgValue = stdConfigTypeTransforms["format"](cfgValue);
+                                                // Create object and set key and value to it.
+                                                let newCfg = {};
+                                                newCfg["format"] = cfgValue;
+                                                // Update guild config in database.
+                                                client.db.setGuildConfig(msg.guild, newCfg).then(() => {
+                                                    msg.channel.send(client.embeds.config.valueSet("format", cfgValue))
+                                                        .then(success => {
+                                                            cMsg.delete(30000);
+                                                            success.delete(30000);
+                                                        })
+                                                        .catch((err) => console.error("[ ERROR ] ", err));
+                                                    utils.setNickname(msg.guild, client);  
+                                                }).catch((err) => msg.channel.send(client.embeds.generalError('Error writing config data to database:', err)));
+                                            } else {
+                                                msg.channel.send(client.embeds.config.incorrectFormat());
+                                            }
+                                        }
+                                    })
                                 break;
                             case '🤖':
                                 break;
--- embeds.js
+++ embeds.js
@@ -125,6 +125,21 @@ module.exports = {
                 .addField("👁 See the current config", "You can see the current config on this server.")
                 .addField("✏ Edit the format of the nickname", "You can edit how the bot displays the members and if it should show online members.")
                 .addField("🤖 Decide whether bots should be counted", "You can decide whether bots should be counted or just members.");
+        },
+
+        enterFormat() {
+            return new discord.RichEmbed()
+                .setColor(0x00bcd4)
+                .setTitle("Enter your desired format")
+                .setDescription("Enter you desrired format. Use `%all` for a placeholder for all (online and offline) members.\nYou can use `%online%` as placeholder just for members that are online. (Note: This spams your audit-log)\n**Note:** You can _not_ use `\"` in your format! Also you _have to_ use at least one placeholder.");
+        },
+
+        incorrectFormat() {
+            return new discord.RichEmbed()
+                .setColor(0xf44336)
+                .setTitle("Your format is incorrect")
+                .addField("First option:", "You forgot to add a placeholder. (Placeholders: `%all%` and `%online%`)")
+                .addField("Second option:", "You used a `\"` which is not allowed.");
         }
     },
 };
