2a90d39ab370eb27ca28c78f7ea81fca9b04c77e
error2507 <37834770+error2507@users.noreply.github.com>
2019-03-10 20:36:49 +0100

    Added new reaction based config. Huge thanks to zekro for doing the DB work

--- commands/config.js
+++ commands/config.js
@@ -1,71 +1,128 @@
-// Standard config values
+const utils = require('../utils');
 const stdConfig = {
     'format':    '%all%',
     'countBots': 1,
 };
 
-// Methdos how to parse the string type parameter from
-// the command to the config value type
 const stdConfigTypeTransforms = {
     // format -> type string -> stirng remains string
     'format': (v) => v,
     // countBots -> type tinyint -> if v == 'true' or '1' result will be 1, else 0
     'countBots': (v) => v.toLowerCase() == 'true' || v == '1' ? 1 : 0,
 };
-
-module.exports.run = (msg, args, client) => {
-    // Getting current guilds config from database.
-    client.db.getGuildConfig(msg.guild).then((guildConfig) => {
-        // Check which keys are set in the config. If a key's value
-        // is 'null', it will be replaced with the standard value
-        // defined above.
-        Object.keys(stdConfig).forEach((k) => {
-            if (guildConfig[k] == null)
-                guildConfig[k] = stdConfig[k];
+module.exports.run = async (msg, args, client) => {
+    msg.channel.send(client.embeds.config.chooseOption())
+        .then(async cMsg => {
+            await cMsg.react("👁");
+            await cMsg.react("✏");
+            await cMsg.react("🤖");
+            let filter = (reaction, user) => (reaction.emoji.name === '👁' || reaction.emoji.name === '✏' || reaction.emoji.name === '🤖') && user.id === msg.author.id;
+            cMsg.awaitReactions(filter, { time: 60000, max: 1 })
+                .then(async reactions => {
+                    if (reactions.size == 1) {
+                        let filter
+                        switch (reactions.first().emoji.name) {
+                            case '👁':
+                                // Removing all reactions
+                                cMsg.clearReactions();
+                                // Getting current guilds config from database.
+                                client.db.getGuildConfig(msg.guild).then((guildConfig) => {
+                                    // Check which keys are set in the config. If a key's value
+                                    // is 'null', it will be replaced with the standard value
+                                    // defined above.
+                                    Object.keys(stdConfig).forEach((k) => {
+                                        if (guildConfig[k] == null)
+                                            guildConfig[k] = stdConfig[k];
+                                    });
+                                    // If guild has no config set, the default config
+                                    // will be used as guild config.
+                                    if (!guildConfig)
+                                        guildConfig = stdConfig;
+                                    // Edit cMsg (which was the menu before) to display current guild config
+                                    cMsg.edit(client.embeds.config.list(guildConfig))
+                                        .then(() => {
+                                            cMsg.delete(30000);
+                                            msg.delete(30000);
+                                        })
+                                        .catch((err) => console.error("[ ERROR ] ", err));
+                                })
+                                break;
+                            case '✏':
+                                cMsg.clearReactions();
+                                cMsg.edit(client.embeds.config.enterFormat());
+                                filter = m => m.author.id == msg.author.id;
+                                msg.channel.awaitMessages(filter, { max: 1, time: 60000 })
+                                    .then(msgs => {
+                                        if (msgs.size == 1) {
+                                            let fMsg = msgs.first();
+                                            if ((fMsg.content.includes("%all%") || fMsg.content.includes("%online%")) && !fMsg.content.includes("\"")) {
+                                                let cfgValue = fMsg.content;
+                                                // Transform config value by keys type transformation function.
+                                                cfgValue = stdConfigTypeTransforms["format"](cfgValue);
+                                                // Create object and set key and value to it.
+                                                let newCfg = {};
+                                                newCfg["format"] = cfgValue;
+                                                // Update guild config in database.
+                                                client.db.setGuildConfig(msg.guild, newCfg).then(() => {
+                                                    msg.channel.send(client.embeds.config.formatSet(cfgValue))
+                                                        .then(success => {
+                                                            msg.delete(30000);
+                                                            fMsg.delete(30000);
+                                                            cMsg.delete(30000);
+                                                            success.delete(30000);
+                                                        })
+                                                        .catch((err) => console.error("[ ERROR ] ", err));
+                                                    utils.setNickname(msg.guild, client);  
+                                                }).catch((err) => msg.channel.send(client.embeds.generalError('Error writing config data to database:', err)));
+                                            } else {
+                                                msg.channel.send(client.embeds.config.incorrectFormat());
+                                            }
+                                        }
+                                    })
+                                break;
+                            case '🤖':
+                                await cMsg.clearReactions();
+                                cMsg.edit(client.embeds.config.botCount());
+                                await cMsg.react("✅");
+                                await cMsg.react("❎");
+                                filter = (reaction, user) => (reaction.emoji.name === '✅' || reaction.emoji.name === '❎') && user.id === msg.author.id;
+                                cMsg.awaitReactions(filter, { time: 60000, max: 1 })
+                                    .then(r => {
+                                        if (r.size == 1) {
+                                            if (r.first().emoji.name == '✅') {
+                                                let cfgValue = args.slice(1).join(' ');
+                                                // Transform config value by keys type transformation function.
+                                                cfgValue = 1;
+                                                // Create object and set key and value to it.
+                                                let newCfg = {};
+                                                newCfg["countBots"] = cfgValue;
+                                                // Update guild config in database.
+                                                client.db.setGuildConfig(msg.guild, newCfg).then(() => {
+                                                    msg.channel.send(client.embeds.config.botCountSet("yes"))
+                                                }).catch((err) => msg.channel.send(client.embeds.generalError('Error writing config data to database:', err)));
+                                            } else {
+                                                let cfgValue = args.slice(1).join(' ');
+                                                // Transform config value by keys type transformation function.
+                                                cfgValue = 0;
+                                                // Create object and set key and value to it.
+                                                let newCfg = {};
+                                                newCfg["countBots"] = cfgValue;
+                                                // Update guild config in database.
+                                                client.db.setGuildConfig(msg.guild, newCfg).then(() => {
+                                                    msg.channel.send(client.embeds.config.botCountSet("no"))
+                                                        .then(success => {
+                                                            msg.delete(30000);
+                                                            cMsg.delete(30000);
+                                                            success.delete(30000);
+                                                        })
+                                                        .catch((err) => console.error("[ ERROR ] ", err));
+                                                }).catch((err) => msg.channel.send(client.embeds.generalError('Error writing config data to database:', err)));
+                                            }
+                                        }
+                                    })
+                                break;
+                        }
+                    }
+                })
         });
-        // If guild has no config set, the default config
-        // will be used as guild config.
-        if (!guildConfig)
-            guildConfig = stdConfig;
-
-        // Display current guilds config as JSON object if
-        // no command arguments are set.
-        if (args.length < 1) {
-            msg.channel.send(client.embeds.config.list(guildConfig))
-                .catch((err) => console.error("[ ERROR ] ", err));
-            return;
-        }
-        
-        // Else, check if config key set in first command argument
-        // matches with any config key. This will be matched non-case-sensitive.
-        let cfgKey = Object.keys(stdConfig)
-            .find((k) => k.toLowerCase() == args[0].toLowerCase());
-        // If no matching key was found, send wrong key error embed.
-        if (!cfgKey) {
-            msg.channel.send(client.embeds.config.errWrongKey(args[0]))
-                .catch((err) => console.error("[ ERROR ] ", err));
-            return;
-        }
-        // If no value is passed as command argument after the config key,
-        // the current set key-value-pair will be displayed in JSON format.
-        if (args.length < 2) {
-            msg.channel.send(client.embeds.config.currValue(guildConfig, args[0]));
-            return;
-        }
-
-        // All values after the first command argument will be interpreted as
-        // config value to set for specified config key.
-        let cfgValue = args.slice(1).join(' ');
-        // Transform config value by keys type transformation function.
-        cfgValue = stdConfigTypeTransforms[cfgKey](cfgValue);
-        // Create object and set key and value to it.
-        let newCfg = {};
-        newCfg[cfgKey] = cfgValue;
-        // Update guild config in database.
-        client.db.setGuildConfig(msg.guild, newCfg).then(() => {
-            msg.channel.send(client.embeds.config.valueSet(cfgKey, cfgValue))
-                .catch((err) => console.error("[ ERROR ] ", err));
-        }).catch((err) => msg.channel.send(client.embeds.generalError('Error writing config data to database:', err)));
-    }).catch((err) => msg.channel.send(client.embeds.generalError('Error getting config data to database:', err)));
-    
-};
+}
