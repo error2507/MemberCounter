05fb0618cc708e4bcfbbf6d31cea8f7f250992c8
Marcel26990 <zockermarcelo03@gmail.com>
2019-08-15 12:19:21 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -31,7 +31,6 @@ module.exports = {
                 let formated = this.formatCount(count, cfg.format);
                 guild.me.setNickname(formated, 'Bot by: https://github.com/error2507/MemberCounter')
                     .then(() => nicknameChanges++);
-                });
                 if (cb) cb(formated);
             });
         });
