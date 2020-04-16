dc6b293d79ce5f5f3ece6972505ebf9c5322f675
Myst <leonhard.schiml@gmx.de>
2020-04-16 22:13:20 +0200

    Update d.js v12
    Updating some links & fixed some typos

--- src/commands/help.js
+++ src/commands/help.js
@@ -10,13 +10,13 @@ module.exports.run = (msg, args, client) => {
         .setAuthor(`MemberCounter Help`, client.user.displayAvatarURL())
         .setDescription('MemberCounter is a simple Discord Bot that shows the current amount of members on a guild as its nickname on this guild.')
 
-        .addField('Commands:', `:white_small_square:\`${prefix}config\` - Display/Edit the current guild configuration. For more details, see [here](https://github.com/error2507/MemberCounter/blob/master/README.md)\n\n:white_small_square:\`${prefix}update\` - Update my nickname manually\n\n:white_small_square:\`${prefix}invite\` - Displays the invite link for me and my support server\n\n:white_small_square:\`${prefix}stats\` - Displays my stats on discordbots.org`)
+        .addField('Commands:', `:white_small_square:\`${prefix}config\` - Display/Edit the current guild configuration. For more details, see [here](https://github.com/error2507/MemberCounter/blob/master/README.md)\n\n:white_small_square:\`${prefix}update\` - Update my nickname manually.\n\n:white_small_square:\`${prefix}invite\` - Displays the invite link for me and my support server.\n\n:white_small_square:\`${prefix}stats\` - Displays my stats on top.gg.`)
 
 
         .setColor('#2980B9')
         .setTimestamp()
         .setFooter(`MemberCounter#0402 by error2507`);
-  //  return msg.channel.send(embed);
+  // return msg.channel.send(embed);
 
 
 };
