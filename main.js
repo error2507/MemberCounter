fed75805f4d3542ec2ea6ed8f91d781f11f191e9
error2507 <37834770+error2507@users.noreply.github.com>
2020-04-18 16:49:00 +0200

    Removed the second ready event

--- main.js
+++ main.js
@@ -58,10 +58,28 @@ function init() {
             });
         }
     });
+
+
+    // The actual stuff to do
+    console.log(`[ INFO ] logged in as ${client.user.tag} (ID: ${client.user.id})`);
+    if (client.config.dblToken && client.config.dblToken != '') {
+        client.shard.fetchClientValues('guilds.size')
+            .then(results => {
+                client.dbl.postStats(results.reduce((prev, memberCount) => prev + memberCount, 0));
+            })
+        setInterval(function() {
+            client.shard.fetchClientValues('guilds.size')
+                .then(results => {
+                    client.dbl.postStats(results.reduce((prev, memberCount) => prev + memberCount, 0));
+                })
+        }, 300000);
+    }
+    client.user.setActivity(client.config.prefix + 'help', { type: 'PLAYING' })
+        .catch((err) => console.error("[ ERROR ] ", err));
 }
 
 client.on('ready', () => {
     init();
-})
+});
 
 client.login(client.config.token);
