ee4c720550fca36f9ab00166a00435b69cd2e1d4
Myst <leonhard.schiml@gmx.de>
2020-04-18 16:33:56 +0200

    Internal logger update

--- src/extensions/logger.js
+++ src/extensions/logger.js
@@ -2,7 +2,7 @@ const chalk = require('chalk');
 const df = require('dateformat');
 
  /**
-  * Register new timeout handler.
+  * Register new logger.
   * @param {string} module     Name of the module.
   * @param {string} message    Message to log.
   * @returns Prints to console.
@@ -11,15 +11,9 @@ const df = require('dateformat');
 module.exports = {
 	info(module, message) {
 		if(!module) {
-			return console.log(chalk.blue('[' + df(new Date(), 'HH:MM:ss') + ']  info  ') + ': ' + message); // + df(new Date(), "dd.mm.yyyy HH:MM:ss") +
+			return console.log(chalk.blue('[' + df(new Date(), 'HH:MM:ss') + ']  info  ') + ': ' + message);
 		}
 		return console.log('[' + df(new Date(), 'HH:MM:ss') + ']' + chalk.blue('  info  ') + '[' + module + ']' + ': ' + message);
-	},
-	warn(module, message) {
-		if(!module) {
-			return console.log('[' + df(new Date(), 'HH:MM:ss') + ']' + chalk.yellow('  warning  ') + ': ' + message);
-		}
-		return console.log('[' + df(new Date(), 'HH:MM:ss') + ']' + chalk.yellow('  warning  ') + '[' + module + ']' + ': ' + message);
 	},
 	error(module, message) {
 		if(!module) {
