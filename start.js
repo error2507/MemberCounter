ca8a04e85aa04392ee8b14b1fc39e84ed5852a79
Myst <34489470+Myst82015@users.noreply.github.com>
2020-04-21 11:39:37 +0200

    Critical hotfix

--- start.js
+++ start.js
@@ -10,7 +10,7 @@ try {
     console.error('[ FATAL ] Failed parsing config.json: ', err);
     process.exit(1);
 }
-
+/*
 const Manager = new ShardingManager('./main.js', {
     shardArgs: args,
     totalShards: 'auto',
@@ -22,3 +22,28 @@ Manager.on('launch', (shard) => {
     console.log(`Launched shard ${shard.id}`);
 });
 
+*/
+const config = require('./config.json');
+const logger = require('./extensions/logger')
+const { ShardingManager } = require('discord.js');
+const manager = new ShardingManager('./main.js', { 
+    shardArgs: ['--ansi', '--color', '--trace-warnings'],
+    respawn: true,
+    totalShards: 'auto',
+    token: config.token 
+});
+
+manager.spawn();
+manager.on('shardCreate', shard => console.log(`[Shard-Manager] Launched shard ${shard.id}`));
+
+manager.on('shardError', shard => console.log(`[Shard-Manager] Shard[${shard.id}]'s Websocket encountered a connection error.`));
+manager.on('shardReady', shard => console.log(`[Shard-Manager] Shard[${shard.id}]'s is ready.`));
+
+manager.on('shardReconnecting', shard => console.log(`[Shard-Manager] Shard[${shard.id}]'s is reconnecting.`));
+manager.on('shardResume', shard => console.log(`[Shard-Manager] Shard[${shard.id}] resumed sucessfully`));
+
+manager.on('shardDisconnect', shard => {
+    
+    console.log(`[Shard-Manager] Shard[${shard.id}] disconnected`)
+    //manager.respawnAll()
+});
