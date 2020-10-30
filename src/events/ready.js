fb147f68a471d79b394fd4df9d04060123aa8d78
error2507 <37834770+error2507@users.noreply.github.com>
2020-10-30 20:00:54 +0100

    cleanup

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,25 +4,14 @@
 	/* Does not run due to init() in main.js */
 
      if (client.config.dblToken && client.config.dblToken != '') {
-        /*client.shard.fetchClientValues('guilds.cache.size')
-            .then(results => {
-                client.dbl.postStats(results.reduce((prev, memberCount) => prev + memberCount, 0));
-            })
-
-            */
         setInterval(function() {
             client.shard.broadcastEval('this.guilds.cache.size')
             //client.shard.fetchClientValues('guilds.cache.size')
                 .then(results => {
                     client.dbl.postStats(results.reduce((prev, memberCount) => prev + memberCount, 0));
                 })
-        }, 1200000 ); // every 20 minutes
+        }, 1200000); // every 20 minutes
      }
     client.user.setActivity(client.config.prefix + 'help', { type: 'PLAYING' })
         .catch((err) => client.logger.error('', err));
-
-
- // client.logger.info('', `Logged in as ${client.user.tag} (ID: ${client.user.id})`)
-
-
- };
+};
