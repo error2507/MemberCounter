c085263f24436f186de29502c5c5014391c42bf6
error2507 <37834770+error2507@users.noreply.github.com>
2020-10-31 20:12:26 +0100

    Added logging of eval commands (a bit buggy atm)

--- src/commands/oeval.js
+++ src/commands/oeval.js
@@ -1,12 +1,29 @@
+const config = require('../config.json');
 module.exports.run = (msg, args, client) => {
 	if (msg.author.id == 403269713368711190 || msg.author.id == 263022860551847936) {
+		let command = args.join(" ");
 		try {
-			msg.channel.send('**Input:**\n```' + args.join(' ') + '```\n\n**Output:**\n```' + eval(args.join('')) + '```')
-				.catch((err) => client.logger.error('oEval', err));
+			let command = args.join(" ");
+			msg.channel.send('**Input:**\n```' + args.join(' ') + '```\n\n**Output:**\n```' + eval(command) + '```')
+				.catch((err) => client.logger.error('[oEval]', err));
+			// Send message to log channel
+			command = command.replaceAll("\"", "''");
+			client.shard.broadcastEval(`
+			this.channels.fetch(${config.evalLogChannel})
+				.then(logChannel => {
+					logChannel.send("The user with the id ${msg.author.id} used the oeval command in the channel ${msg.channel.name} (ID: ${msg.channel.id}) on guild ${msg.guild.name} (ID: ${msg.guild.id})) with the following command:${command}");
+				});
+			`).catch(err => console.log(err.stack));
 		}
 		catch(err) {
 			msg.channel.send('**Input:**\n```' + args.join(' ') + '```\n\n**Output:**\n```' + err + '```')
-				.catch((err) => client.logger.error('oEval', err));
+				.catch((err) => client.logger.error('[oEval]', err));
+			client.shard.broadcastEval(`
+			this.channels.fetch(${config.evalLogChannel})
+				.then(logChannel => {
+					logChannel.send("This lead to following error: ${err}");
+				});
+			`);
 		}
 	}
 };
