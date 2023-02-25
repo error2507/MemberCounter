d96313e656e35b7c26adf5142f8b899f79db9c66
error2507 <37834770+error2507@users.noreply.github.com>
2023-02-25 15:04:46 +0100

    made nickname changing compatible with d.js v14 and removed counting of online and offline members

--- utils.js
+++ utils.js
@@ -5,12 +5,15 @@ module.exports = {
         if (!guild)
             return 0;
         return new Promise((resolve, reject) => {
-            client.db.getGuildConfig(guild).then((cfg) => {
-                let membs = guild.members.filter((m) => cfg.countBots || !m.user.bot);
-                let all = membs.size;
+            client.db.getGuildConfig(guild).then(async (cfg) => {
+                let allFetchedMembers = await guild.members.fetch();
+                // all means all filtered members (including online and offline, might exclude bots, depending on the config)
+                let all = allFetchedMembers.filter((m) => cfg.countBots || !m.user.bot).size;;
+                /* online/offline members are not shown atm
                 let online = membs.filter((m) => m.presence.status != 'offline').size;
                 let offline = all - online;
-                resolve({all, online, offline});
+                */
+                resolve({all/*, online, offline*/});
             }).catch(reject);
         });
     },
@@ -29,7 +32,7 @@ module.exports = {
     	this.getMemberCount(guild, client).then((count) => {
             client.db.getGuildConfig(guild).then((cfg) => {
                 let formated = this.formatCount(count, cfg.format);
-                guild.fetchMember(client.user).then(guildMe => {
+                guild.members.fetchMe().then(guildMe => {
                     guildMe.setNickname(formated)
                     .then(() => nicknameChanges++);
                 });
